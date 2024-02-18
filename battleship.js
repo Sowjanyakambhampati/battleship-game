38ec3850857680e55e8a377abc39738b84de430c
Sowjanyakambhampati <93071280+Sowjanyakambhampati@users.noreply.github.com>
2024-02-18 01:23:45 +0100

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -16,7 +16,7 @@ for (i = 0; i < cols; i++) {
     // give each div element a unique id based on its row and column, like "s00"
     square.id = "s" + j + i;
 
-    square.innerHTML += square.id;
+    //square.innerHTML += square.id;
 
     // set each grid square's coordinates: multiples of the current row or column number
     var topPosition = j * squareSize;
@@ -29,12 +29,12 @@ for (i = 0; i < cols; i++) {
 }
 
 /* 
-   there are 17 hits to be made in order to win the game:
+   there are 15 hits to be made in order to win the game:
       Carrier     - 5 hits
       Battleship  - 4 hits
-      Destroyer   - 3 hits
-      Submarine   - 3 hits
-      Patrol Boat - 2 hits
+      Cruiser     - 3 hits
+      Destroyer   - 2 hits
+      submarine   - 1 hit
 */
 var hitCount = 0;
 
@@ -48,16 +48,18 @@ var gameBoard = [];
 
 const shipLengths = [5, 4, 3, 2, 1];
 
-var computerShips =[];
+var computerShips = [];
 
 var hitCells = [];
 
-var minimumHits =0;
+var minimumHits = 0;
 
-for(let i=0;i<shipLengths.length ;i++){
-	minimumHits +=shipLengths[i];
+for (let i = 0; i < shipLengths.length; i++) {
+  minimumHits += shipLengths[i];
 }
 
+
+// Place Ships Randomly 
 placeShips(gameBoard);
 
 function placeShips(board) {
@@ -78,12 +80,12 @@ function placeShips(board) {
     if (isHorizontal) {
       for (let i = 0; i < length; i++) {
         board[x][y + i] = 1;
-        computerShip.push("s" + x + (y+i));
+        computerShip.push("s" + x + (y + i));
       }
     } else {
       for (let i = 0; i < length; i++) {
         board[x + i][y] = 1;
-        computerShip.push("s" + (x+i) + y);
+        computerShip.push("s" + (x + i) + y);
       }
     }
     computerShips.push(computerShip);
@@ -106,14 +108,14 @@ function isValidPosition(board, x, y, length, isHorizontal) {
 // set event listener for all elements in gameboard, run fireTorpedo function when square is clicked
 gameBoardContainer.addEventListener("click", fireTorpedo, false);
 
-var totalHits =0;
+var totalHits = 0;
 
 function fireTorpedo(e) {
   // if item clicked (e.target) is not the parent element on which the event listener was set (e.currentTarget)
   if (e.target !== e.currentTarget) {
-	
-	//count total hits
-	totalHits +=1;
+
+    //count total hits
+    totalHits += 1;
 
     // extract row and column # from the HTML element's id
     var row = e.target.id.substring(1, 2);
@@ -135,80 +137,71 @@ function fireTorpedo(e) {
 
       // increment hitCount each time a ship is hit
       hitCount++;
-          if (hitCount == minimumHits) {
+      if (hitCount == minimumHits) {
         alert("All enemy battleships have been defeated! You win! and you took " + totalHits + " hits");
       }
-
       // if player clicks a square that's been previously hit, let them know
     } else if (gameBoard[row][col] > 1) {
       alert("You already fired at this location!");
     }
     nextTurn();
-
-    
   }
   e.stopPropagation();
 }
 
 const drawCells = (cells, target) => {
   let color = "grey"
-  if (target == "enemy"){
-
-    switch(cells.length) {
-
+  if (target == "enemy") {
+    switch (cells.length) {
       case 5:
         var element = document.getElementsByClassName("board-components-111");
-        document.getElementById("carrier-1").style.backgroundColor= "red";
-        document.getElementById("carrier-2").style.backgroundColor= "red";
-        document.getElementById("carrier-3").style.backgroundColor= "red";
-        document.getElementById("carrier-4").style.backgroundColor= "red";
-        document.getElementById("carrier-5").style.backgroundColor= "red";
+        document.getElementById("carrier-1").style.backgroundColor = "red";
+        document.getElementById("carrier-2").style.backgroundColor = "red";
+        document.getElementById("carrier-3").style.backgroundColor = "red";
+        document.getElementById("carrier-4").style.backgroundColor = "red";
+        document.getElementById("carrier-5").style.backgroundColor = "red";
         break;
       case 4:
         var element = document.getElementsByClassName("board-components-110");
-        document.getElementById("battleship-1").style.backgroundColor= "red";
-        document.getElementById("battleship-2").style.backgroundColor= "red";
-        document.getElementById("battleship-3").style.backgroundColor= "red";
-        document.getElementById("battleship-4").style.backgroundColor= "red";
+        document.getElementById("battleship-1").style.backgroundColor = "red";
+        document.getElementById("battleship-2").style.backgroundColor = "red";
+        document.getElementById("battleship-3").style.backgroundColor = "red";
+        document.getElementById("battleship-4").style.backgroundColor = "red";
         break;
       case 3:
         var element = document.getElementsByClassName("board-components-108");
-        document.getElementById("cruiser-1").style.backgroundColor= "red";
-        document.getElementById("cruiser-2").style.backgroundColor= "red";
-        document.getElementById("cruiser-3").style.backgroundColor= "red";
+        document.getElementById("cruiser-1").style.backgroundColor = "red";
+        document.getElementById("cruiser-2").style.backgroundColor = "red";
+        document.getElementById("cruiser-3").style.backgroundColor = "red";
         break;
       case 2:
         var element = document.getElementsByClassName("board-components-109");
-        document.getElementById("destroyer-1").style.backgroundColor= "red";
-        document.getElementById("destroyer-2").style.backgroundColor= "red";
+        document.getElementById("destroyer-1").style.backgroundColor = "red";
+        document.getElementById("destroyer-2").style.backgroundColor = "red";
         break;
       case 1:
-        document.getElementById("submarine").style.backgroundColor= "red";
+        document.getElementById("submarine").style.backgroundColor = "red";
         break;
-        default:
-          break;
+      default:
+        break;
 
     }
-    
+
 
   }
 }
 
-
 const nextTurn = () => {
-
   // Draw only Enemy Ships that have completely sunk
   computerShips.forEach(ship => {
-
-      // Get cells that exist in the ship
-      const hitCellsShips = hitCells.filter(cell => ship.includes(cell))
-
-      // If the whole cells exist, it means the ship is completely sunk, we can draw it in the screen
-      if (hitCellsShips.length == ship.length){
-          drawCells(ship, "enemy")
-      }
+    // Get cells that exist in the ship
+    const hitCellsShips = hitCells.filter(cell => ship.includes(cell))
+    // If the whole cells exist, it means the ship is completely sunk, we can draw it in the screen
+    if (hitCellsShips.length == ship.length) {
+      drawCells(ship, "enemy")
+    }
   })
-  drawUIShips(computerShips)
+  //drawUIShips(computerShips)
 }
 
 // Ships need to be configured
@@ -216,24 +209,21 @@ const drawUIShips = (ships) => {
   const UIships = document.querySelector("#ships")
   UIships.innerHTML = ''
   ships.forEach(ship => {
-      ship.forEach(cell => {
-          if (hitCells.includes(cell)){
-              UIships.innerHTML += `<span style="color: red">${cell}</span>`
-          }
-          else {
-              UIships.innerHTML += cell
-          }
-      })
-      UIships.innerHTML += `<br>`
+    ship.forEach(cell => {
+      if (hitCells.includes(cell)) {
+        UIships.innerHTML += `<span style="color: red">${cell}</span>`
+      }
+      else {
+        UIships.innerHTML += cell
+      }
+    })
+    UIships.innerHTML += `<br>`
   })
 
 }
 
-
 window.onload = () => {
-
   nextTurn();
-
 }
 
 
